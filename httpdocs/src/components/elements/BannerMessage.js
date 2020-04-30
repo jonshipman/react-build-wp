7c423a142b44d70d9ff2e5b1fe31b93226b44aeb
Jon Shipman <jon@jonshipman.com>
2020-04-30 13:30:05 -0500

    check prop messages instead of length

--- httpdocs/src/components/elements/BannerMessage.js
+++ httpdocs/src/components/elements/BannerMessage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const BannerMessage = (props) => (
   <>
-    {props.message.length > 0 && (
+    {props.message && (
       <div className="banner-message"><Link to="test">{props.message}</Link></div>
     )}
   </>
