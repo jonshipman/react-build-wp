14920328d61f34c0295d1453b603bbc8dc843d6c
Jon Shipman <jon@jonshipman.com>
2020-05-19 15:30:26 -0500

    script for View Link -- forgot in prev commit

---
+++ wordpress/wp-content/themes/headless-wp/assets/editor-scripts.js
@@ -0,0 +1,27 @@
+(function(wp, d) {
+    const { registerPlugin } = wp.plugins;
+    const { createElement } = wp.element;
+    const { withSelect } = wp.data;
+    const { compose } = wp.compose;
+
+    var ModifyPostLinks = compose(
+        withSelect(select => {
+            return {
+                permalinkParts: select('core/editor').getPermalinkParts()
+            }
+        })
+    )(({ permalinkParts }) => {
+        const links = d.querySelectorAll('.edit-post-post-link__link')
+        links.forEach(e => {
+            let name = `${HeadlessWp.frontend_origin}/${permalinkParts.postName}`;
+            e.href = name;
+            e.innerHTML = name;
+        });
+
+        return null;
+    });
+
+    registerPlugin('headless-wp-editor-scripts', {
+        render:  () => createElement(ModifyPostLinks)
+    });
+})(window.wp, document);
