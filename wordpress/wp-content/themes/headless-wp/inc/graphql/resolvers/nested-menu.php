a7fa1a4b0f7f704c0609247e74ffb77d05c68e4a
Jon Shipman <jon@jonshipman.com>
2020-05-29 17:03:13 -0500

    var checks

--- wordpress/wp-content/themes/headless-wp/inc/graphql/resolvers/nested-menu.php
+++ wordpress/wp-content/themes/headless-wp/inc/graphql/resolvers/nested-menu.php
@@ -19,38 +19,50 @@ function postlight_headless_wp_get_menu( $name ) {
     $resolve = array();
 
     $theme_locations = get_nav_menu_locations();
-    $menu_obj        = get_term( $theme_locations[ $name ], 'nav_menu' );
-    $menu_name       = $menu_obj->name;
-    $menu_items      = wp_get_nav_menu_items( $menu_name );
 
-    $children_array = array();
-    foreach ( $menu_items as $item ) {
-        $children_array[ $item->ID ] = (array) $item;
-    }
+    if ( isset( $theme_locations[ $name ] ) ) {
+        $menu_obj = get_term( $theme_locations[ $name ], 'nav_menu' );
 
-    foreach ( $menu_items as $item ) {
-        if ( isset( $children_array[ $item->menu_item_parent ] ) ) {
-            if ( ! isset( $children_array[ $item->menu_item_parent ]['children'] ) ) {
-                $children_array[ $item->menu_item_parent ]['children'] = array();
+        if ( $menu_obj && ! is_wp_error( $menu_obj ) ) {
+            $menu_name  = $menu_obj->name;
+            $menu_items = wp_get_nav_menu_items( $menu_name );
+
+            $children_array = array();
+                if ( ! empty( $menu_items ) ) {
+                    foreach ( $menu_items as $item ) {
+                    $children_array[ $item->ID ] = (array) $item;
+                }
             }
 
-            $children_array[ $item->menu_item_parent ]['children'][] = $item->ID;
+            if ( ! empty( $menu_items ) ) {
+                foreach ( $menu_items as $item ) {
+                    if ( isset( $children_array[ $item->menu_item_parent ] ) ) {
+                        if ( ! isset( $children_array[ $item->menu_item_parent ]['children'] ) ) {
+                            $children_array[ $item->menu_item_parent ]['children'] = array();
+                        }
+
+                        $children_array[ $item->menu_item_parent ]['children'][] = $item->ID;
+                    }
+                }
+            }
+
+            if ( ! empty( $menu_items ) ) {
+                foreach ( $menu_items as $item ) {
+                    $converted_item = postlight_headless_wp_nav_menu_item( $item );
+
+                    if ( isset( $children_array[ $item->ID ]['children'] ) ) {
+                        $converted_item['children']    = $children_array[ $item->ID ]['children'];
+                        $converted_item['hasChildren'] = true;
+                    } else {
+                        $converted_item['hasChildren'] = false;
+                    }
+
+                    $resolve[ $counter ] = $converted_item;
+
+                    $counter++;
+                }
+            }
         }
-    }
-
-    foreach ( $menu_items as $item ) {
-        $converted_item = postlight_headless_wp_nav_menu_item( $item );
-
-        if ( isset( $children_array[ $item->ID ]['children'] ) ) {
-            $converted_item['children']    = $children_array[ $item->ID ]['children'];
-            $converted_item['hasChildren'] = true;
-        } else {
-            $converted_item['hasChildren'] = false;
-        }
-
-        $resolve[ $counter ] = $converted_item;
-
-        $counter++;
     }
 
     return $resolve;
