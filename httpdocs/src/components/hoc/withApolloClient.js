9d4e5bf6223bcc7727e4357381157684ef6b566a
Jon Shipman <jon@jonshipman.com>
2020-05-29 08:45:58 -0500

    stops components from attempting a mount after unmounted

--- httpdocs/src/components/hoc/withApolloClient.js
+++ httpdocs/src/components/hoc/withApolloClient.js
@@ -3,11 +3,21 @@ import { ApolloConsumer } from '@apollo/client';
 
 export default WrappedComponent => {
   return class extends Component {
+    _mounted = false;
+
+    componentDidMount() {
+      this._mounted = true;
+    }
+
+    componentWillUnmount() {
+      this._mounted = false;
+    }
+
     render() {
       return (
         <ApolloConsumer>
           {client => {
-            return <WrappedComponent client={client} { ...this.props } />
+            return this._mounted ? <WrappedComponent client={client} { ...this.props } /> : null;
           }}
         </ApolloConsumer>
       );
