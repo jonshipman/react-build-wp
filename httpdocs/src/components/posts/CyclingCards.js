b9c48db1dd9e8830f31dc9064664a5a50a18e24d
Jon Shipman <jon@jonshipman.com>
2020-06-03 11:04:35 -0500

    stops ui blink

--- httpdocs/src/components/posts/CyclingCards.js
+++ httpdocs/src/components/posts/CyclingCards.js
@@ -38,14 +38,9 @@ const QUERY = gql`
 
 const Wrapper = ({ cards, children, ...rest }) => {
   return (
-    <div className="w-100 h-100 bg-dark-gray pa4 tc flex-l items-center-l">
-      <div className="w-100-l">
-        <h3><span className="fw4 f2 white">Our Company <span className="fw7">Advantage</span></span></h3>
-        <ul className="cards-cycling list pl0 flex-l flex-wrap-l mw7 center" { ...rest }>
-          {cards.map(card => children(card))}
-        </ul>
-      </div>
-    </div>
+    <ul className="cards-cycling list pl0 flex-l flex-wrap-l mw7 center" { ...rest }>
+      {cards.map(card => children(card))}
+    </ul>
   );
 };
 
@@ -53,22 +48,27 @@ const Wrapper = ({ cards, children, ...rest }) => {
  * Cycling cards
  */
 export default () => (
-  <RotatingCards query={QUERY} wrapper={Wrapper}>
-    {card => (
-      <li className="w-100 w-50-l card pa3" key={card.node.id}>
-        <div className="w-100 bg-white aspect-ratio-l aspect-ratio--16x9-l">
-          <div className="card--inner w-100 absolute-l z-1 pa2 pa0-l">
-            <div className="w-100 grow">
-              <Image
-                width={64}
-                height={64}
-                alt="Replace with featured or custom meta (paste svg into acf textarea)"
-              />
+  <div className="w-100 h-100 bg-dark-gray pa4 tc flex-l items-center-l">
+    <div className="w-100-l">
+      <h3><span className="fw4 f2 white">Our Company <span className="fw7">Advantage</span></span></h3>
+      <RotatingCards query={QUERY} wrapper={Wrapper}>
+        {card => (
+          <li className="w-100 w-50-l card pa3" key={card.node.id}>
+            <div className="w-100 bg-white aspect-ratio-l aspect-ratio--16x9-l">
+              <div className="card--inner w-100 absolute-l z-1 pa2 pa0-l">
+                <div className="w-100 grow">
+                  <Image
+                    width={64}
+                    height={64}
+                    alt="Replace with featured or custom meta (paste svg into acf textarea)"
+                  />
+                </div>
+                <div className="mt2 f7 fw7 w-90 center">{card.node.title}</div>
+              </div>
             </div>
-            <div className="mt2 f7 fw7 w-90 center">{card.node.title}</div>
-          </div>
-        </div>
-      </li>
-    )}
-  </RotatingCards>
+          </li>
+        )}
+      </RotatingCards>
+    </div>
+  </div>
 );
