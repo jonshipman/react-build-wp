27c07aeb75d1abe7dd7298604d18a55b772cb7bb
Jon Shipman <jon@jonshipman.com>
2020-06-10 16:38:58 -0500

    added push for sticky header

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
@@ -16,27 +16,44 @@ const openMenu = () => {
   }
 }
 
-const Header = ({ sticky }) => (
-  <header id="header" className={`w-100 z-2 ${sticky ? 'absolute fixed-l top-0' : 'relative'}`}>
-    <div className="bg-white bg-white-80-l">
-      <nav>
-        <PageWidth className="dt-l">
-          <div className="brand flex items-center tc dtc-l v-mid-l tl-l">
-            <div className="mobile-toggle pr3 pv3 db dn-l" onClick={openMenu}>
-              <div/><div/><div/>
-            </div>
-            <Link to="/" className="dib border-box mv3" onClick={() => document.getElementById('menu-header-menu').classList.add('dn')}>
-              <Logo className="w4 nb1"/>
-            </Link>
-          </div>
-          <div className="db tc dtc-l v-mid-l tr-l">
-            <NestedMenu location="header-menu" className="dn ma0 pl0 list db-l" anchorOnclick={openMenu} />
-          </div>
-        </PageWidth>
-      </nav>
-    </div>
-  </header>
-)
+const Header = ({ sticky }) => {
+  const headerRef = useRef();
+  const spacerRef = useRef();
+
+  useEffect(() => {
+    if (sticky && spacerRef.current) {
+      spacerRef.current.style.height = `${headerRef?.current?.clientHeight}px`;
+    }
+  }, [sticky]);
+
+  return (
+    <header id="header">
+      <div ref={headerRef} className={`w-100 z-2 ${sticky ? 'absolute fixed-l top-0' : 'relative'}`}>
+        <div className="bg-white bg-white-80-l">
+          <nav>
+            <PageWidth className="dt-l">
+              <div className="brand flex items-center tc dtc-l v-mid-l tl-l">
+                <div className="mobile-toggle pr3 pv3 db dn-l" onClick={openMenu}>
+                  <div/><div/><div/>
+                </div>
+                <Link to="/" className="dib border-box mv3" onClick={() => document.getElementById('menu-header-menu').classList.add('dn')}>
+                  <Logo className="w4 nb1"/>
+                </Link>
+              </div>
+              <div className="db tc dtc-l v-mid-l tr-l">
+                <NestedMenu location="header-menu" className="dn ma0 pl0 list db-l" anchorOnclick={openMenu} />
+              </div>
+            </PageWidth>
+          </nav>
+        </div>
+      </div>
+
+      {sticky && (
+        <div ref={spacerRef} />
+      )}
+    </header>
+  );
+}
 
 export default () => (
   <Switch>
