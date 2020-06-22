e296a8064d20c0761a77677b961d4a5b50dfd835
Jon Shipman <jon@jonshipman.com>
2020-06-22 09:43:28 -0500

    concatenate form fields with components

--- react/src/components/forms/Base.js
+++ react/src/components/forms/Base.js
@@ -1,4 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Field, ErrorMessage } from 'formik';
+
+import FormError from '../elements/FormError';
 
 class FormBase {
   getButton = () => {
@@ -29,23 +32,6 @@ class FormBase {
     return this.form.fields[key].errorMessage;
   }
 
-  getLocationQueryField = () => {
-    const { fields } = this.form;
-    let ret = null;
-
-    Object.entries(fields).map(([key, value]) => {
-      let { locationQuery } = value;
-
-      if (locationQuery) {
-        ret = key;
-      }
-
-      return null;
-    });
-
-    return ret;
-  }
-
   buildState = () => {
     const { fields } = this.form;
     const emptyFormValues = {};
@@ -59,16 +45,39 @@ class FormBase {
     return emptyFormValues;
   }
 
+  templates = {
+    Text: ({ key, name, label, className='', type='text', ...props }) => (
+      <div className={`form-group w-100 mb4 drop-last-mb ${className}`} { ...props }>
+        <label htmlFor={name} className="fw7 ttu db w-100 mb2 pl2">{$label}</label>
+        <Field type={type} id={name} name={name} placeholder={$label} className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2" />
+        <ErrorMessage name={name} component={FormError} />
+      </div>
+    ),
+    Select: ({ key, name, label, className='', options, ...props }) => (
+      <div className={`form-group w-100 mb4 drop-last-mb ${className}`} { ...props }>
+        <label htmlFor={name} className="fw7 ttu db w-100 mb2 pl2">{$label}</label>
+        <Field className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2" as="select" name={name}>
+          <option value=""></option>
+          {options.map(opt => <option key={name + opt.value} value={opt.value}>{opt.label}</option>)}
+        </Field>
+        <ErrorMessage name={name} component={FormError} />
+      </div>
+    ),
+    Textarea: ({ key, name, label, className='', ...props }) => (
+      <div className={`form-group w-100 mb4 drop-last-mb ${className}`} { ...props }>
+        <label htmlFor={name} className="fw7 ttu db w-100 mb2 pl2">{label}</label>
+        <Field component="textarea" id={name} name={name} placeholder={label} className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2 h3" />
+        <ErrorMessage name={name} component={FormError} />
+      </div>
+    ),
+  }
+
   component = () => {
     const { fields } = this.form;
     return (
       <>
         {Object.entries(fields).map(([key, value]) => {
-          return (
-            <Fragment key={key}>
-              <value.component />
-            </Fragment>
-          );
+          return <value.component key={key} />;
         })}
       </>
     );
--- react/src/components/forms/DefaultForm.js
+++ react/src/components/forms/DefaultForm.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { gql } from '@apollo/client';
-import { Field, ErrorMessage } from 'formik';
 
 import { isPhone, isEmail } from '../utils/String';
 import Base from './Base';
-import FormError from '../elements/FormError';
 
 class Form extends Base {
   name = 'default';
@@ -41,52 +39,28 @@ class Form extends Base {
       `,
     fields: {
       yourName: {
-        component: () => (
-          <div className="form-group w-100 mb4 fl-l f5">
-            <label htmlFor="yourName" className="fw7 ttu db w-100 mb2 pl2">Full Name</label>
-            <Field type="text" id="yourName" name="yourName" placeholder="Your Name" className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2" />
-            <ErrorMessage name="yourName" component={FormError} />
-          </div>
-        ),
+        component: () => <this.templates.Text name="yourName" label="Your Name" />,
         validity: v => {
           return v && v.length > 0;
         },
         errorMessage: 'Required.'
       },
       email: {
-        component: () => (
-          <div className="form-group mb4 w-100 w-50-l fl-l f5 pr3-l">
-            <label htmlFor="email" className="fw7 ttu db w-100 mb2 pl2">Email Address</label>
-            <Field type="email" id="email" name="email" placeholder="Your Email" className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2" />
-            <ErrorMessage name="email" component={FormError} />
-          </div>
-        ),
+        component: () => <this.templates.Text className="w-50-l fl-l" name="email" type="email" label="Your Email" />,
         validity: v => {
           return isEmail(v);
         },
         errorMessage: 'Invalid email address.'
       },
       phone: {
-        component: () => (
-          <div className="form-group mb4 w-100 w-50-l fl-l f5 pl3-l">
-            <label htmlFor="phone" className="fw7 ttu db w-100 mb2 pl2">Phone</label>
-            <Field type="text" id="phone" name="phone" placeholder="Your Phone" className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2" />
-            <ErrorMessage name="phone" component={FormError} />
-          </div>
-        ),
+        component: () => <this.templates.Text className="w-50-l fl-l" name="phone" type="tel" label="Your Phone" />,
         validity: v => {
           return isPhone(v);
         },
         errorMessage: 'Invalid phone number.'
       },
       message: {
-        component: () => (
-          <div className="form-group w-100 mb4 fl-l f5">
-            <label htmlFor="message" className="fw7 ttu db w-100 mb2 pl2">Message</label>
-            <Field component="textarea" id="message" name="message" placeholder="Your Message" className="w-100 b--light-silver br0 bb-1 bl-0 br-0 bt-0 pa pl2 pb2 h3" />
-            <ErrorMessage name="message" component={FormError} />
-          </div>
-        ),
+        component: () => <this.templates.Textarea name="message" label="Your Message" />,
         validity: () => {
           return true;
         }
