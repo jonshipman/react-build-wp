cc6c4f51a0c2879ede437ff1e597e6c47ecc52c3
Jon Shipman <jon@jonshipman.com>
2020-06-23 16:40:31 -0500

    added fields for the Total theme (wpex)

--- wordpress/wp-content/themes/headless-wp/inc/graphql/resolvers/post.php
+++ wordpress/wp-content/themes/headless-wp/inc/graphql/resolvers/post.php
@@ -7,6 +7,8 @@
 
 use \WPGraphQL\Data\DataSource;
 
+require_once __DIR__ . '/../types/post.php';
+
 add_action(
     'graphql_register_types',
     function () {
@@ -50,6 +52,35 @@ add_action(
             );
 
             register_graphql_field( 'ContentNode', $name, $config );
+
+            register_graphql_field(
+                'ContentNode',
+                'totalMeta',
+                array(
+                    'type' => 'TotalFields',
+                    'description' => 'Total Post Meta fields',
+                    'resolve' => function( $post ) {
+                        $r = array(
+                            'titleBackground' => null,
+                            'titleBackgroundPosition' => get_post_meta( $post->ID, 'wpex_post_title_background_position', true ) ?: null,
+                            'subTitle' => get_post_meta( $post->ID, 'wpex_post_subheading', true ) ?: null,
+                            'titleHeight' => get_post_meta( $post->ID, 'wpex_post_title_height', true ) ?: null,
+                            'titleBackgroundOverlay' => get_post_meta( $post->ID, 'wpex_post_title_background_overlay', true ) ?: null,
+                            'titleDisabled' => get_post_meta( $post->ID, 'wpex_disable_title', true ) === 'on',
+                        );
+
+                        $id = get_post_meta( $post->ID, 'wpex_post_title_background_redux', true );
+                        if ( $id && 'background-image' === get_post_meta( $post->ID, 'wpex_post_title_style', true ) ) {
+                            $src = wp_get_attachment_image_src( $id, 'full' );
+                            if ( $src ) {
+                                $r['titleBackground'] = $src[0];
+                            }
+                        }
+
+                        return $r;
+                    },
+                )
+            );
         }
     }
 );
