0952f9d92fcbb4d26b599de33c53b57477bfaa51
Jon Shipman <jon@jonshipman.com>
2020-06-30 13:40:57 -0500

    styles are no longer hardcoded into template for menus

--- react/src/components/elements/Menu.js
+++ react/src/components/elements/Menu.js
@@ -1,52 +1,36 @@
-import React, { forwardRef } from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { NavLink } from 'react-router-dom';
+import React, { forwardRef } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { NavLink } from "react-router-dom";
 
 /**
  * Loading functional component that loads the skeleton, error,
  * or finished component based on results.
  */
-const Menu = forwardRef(({ classNames, ...props }, ref) => {
-  let _classNames = { ...defaultClasses, ...classNames };
-
+const Menu = forwardRef((props, ref) => {
   const { error, loading, data } = useQuery(MENU_QUERY, {
-    variables: { location: props.location }
+    variables: { location: props.location },
   });
 
-  return <OnQueryFinished
-    forwardedRef={ref}
-    loading={loading}
-    error={error}
-    { ...props }
-    classNames={_classNames}
-    menuItems={data?.menus?.nodes?.length > 0 && data.menus.nodes[0].menuItems?.nodes?.length > 0 ? data.menus.nodes[0].menuItems.nodes : []}
-  />
+  return (
+    <OnQueryFinished
+      forwardedRef={ref}
+      loading={loading}
+      error={error}
+      {...props}
+      menuItems={
+        data?.menus?.nodes?.length > 0 &&
+        data.menus.nodes[0].menuItems?.nodes?.length > 0
+          ? data.menus.nodes[0].menuItems.nodes
+          : []
+      }
+    />
+  );
 });
 
-/**
- * Default properties.
- */
-const defaultClasses = {
-  li: [
-    'db dib-l relative z-1 hover-z-2 pv3 drop-last-child-pr',
-    'pa2 relative z-1 hover-z-2',
-    'pa2 nowrap'
-  ],
-  a: [
-    'fw4 hover-blue no-underline color-inherit dib pv2 ph3',
-    'hover-blue dark-gray db'
-  ],
-  submenu: [
-    'tl-l list pl0 absolute-l pv2 top-100-l left-0-l bg-white ba b--light-gray w5',
-    'list ph3 absolute-l top-0-l left-100-l bg-white ba b--light-gray'
-  ],
-};
-
 Menu.defaultProps = {
-  location: 'HEADER_MENU',
+  location: "HEADER_MENU",
   ignoreClasses: false,
-  classNames: defaultClasses,
-  anchorOnclick: () => {}
+  anchorOnclick: () => {},
 };
 
 /**
@@ -54,7 +38,7 @@ Menu.defaultProps = {
  */
 const MENU_QUERY = gql`
   query MenuQuery($location: MenuLocationEnum!) {
-    menus(where: {location: $location}) {
+    menus(where: { location: $location }) {
       nodes {
         id
         menuItems(first: 100) {
@@ -87,20 +71,21 @@ const MENU_QUERY = gql`
 const OnQueryFinished = ({ forwardedRef, loading, ...props }) => (
   <ul
     ref={forwardedRef}
-    id={`menu-${props.location.toLowerCase().replace('_','-')}`}
+    id={`menu-${props.location.toLowerCase().replace("_", "-")}`}
     className={`nested-menu ${props.className}`}
-    style={{touchAction: 'pan-y'}}
+    style={{ touchAction: "pan-y" }}
   >
-    {loading || props.error?.message
-    ? <Skeleton { ...props } />
-    : props.menuItems.map(menuItem => {
+    {loading || props.error?.message ? (
+      <Skeleton {...props} />
+    ) : (
+      props.menuItems.map((menuItem) => {
         if (null === menuItem.parentId) {
-          return <ChildItem key={menuItem.id} menuItem={menuItem} { ...props } />
+          return <ChildItem key={menuItem.id} menuItem={menuItem} {...props} />;
         } else {
           return null;
         }
       })
-    }
+    )}
   </ul>
 );
 
@@ -108,64 +93,49 @@ const OnQueryFinished = ({ forwardedRef, loading, ...props }) => (
  * Child item that loops to created the nested menu.
  */
 const ChildItem = ({ menuItem, level, ...props }) => {
-  const { menuItems, classNames } = props;
+  const { menuItems } = props;
   const hasChildren = menuItem?.childItems?.nodes?.length > 0;
   let localLevel = level ? level + 1 : 1;
   let new_children = [];
 
   if (hasChildren) {
-    menuItem.childItems.nodes.forEach(childItem => {
-      new_children.push(menuItems.find(i => i.id === childItem.id));
+    menuItem.childItems.nodes.forEach((childItem) => {
+      new_children.push(menuItems.find((i) => i.id === childItem.id));
     });
-  }
-
-  let liClassName = '';
-  let aClassName = '';
-  let submenuClassName = '';
-
-  if (!props.ignoreClasses) {
-    liClassName = `menu-item ${(hasChildren ? ' has-children' : '')} level-${localLevel}`;
-    submenuClassName = 'dn';
-
-    if (classNames.li[localLevel-1]) {
-      liClassName += ' ' + classNames.li[localLevel-1];
-    }
-
-    if (classNames.a[localLevel-1]) {
-      aClassName += ' ' + classNames.a[localLevel-1];
-    }
-
-    if (classNames.submenu[localLevel-1]) {
-      submenuClassName += ' ' + classNames.submenu[localLevel-1];
-    }
   }
 
   return (
     <li
-      id={'menu-item-' + menuItem.databaseId}
-      key={ menuItem.id }
-      className={`${liClassName} ${menuItem.cssClasses || ''}`}
+      id={"menu-item-" + menuItem.databaseId}
+      key={menuItem.id}
+      className={`menu-item ${
+        hasChildren ? " has-children" : ""
+      } level-${localLevel} ${menuItem.cssClasses || ""}`}
     >
-      {'MenuItem' === menuItem.connectedObject.__typename
-        ?
-          <a href={menuItem.url} className={aClassName} rel="nofollow noopen">
-            <span className="link-inner">{menuItem.label}</span>
-          </a>
-        :
-          <NavLink exact to={menuItem.url} className={aClassName} onClick={props.anchorOnclick} activeClassName="current-item">
-            <span className="link-inner">{menuItem.label}</span>
-          </NavLink>
-      }
+      {"MenuItem" === menuItem.connectedObject.__typename ? (
+        <a href={menuItem.url} rel="nofollow noopen">
+          <span className="link-inner">{menuItem.label}</span>
+        </a>
+      ) : (
+        <NavLink
+          exact
+          to={menuItem.url}
+          onClick={props.anchorOnclick}
+          activeClassName="current-item"
+        >
+          <span className="link-inner">{menuItem.label}</span>
+        </NavLink>
+      )}
       {hasChildren ? (
-        <ul className={`sub-menu dn ${submenuClassName}`}>
-          {new_children.map(m => {
+        <ul className="sub-menu dn">
+          {new_children.map((m) => {
             if (m.parentId === menuItem.id) {
               return (
                 <ChildItem
                   key={m.id}
                   menuItem={m}
                   level={localLevel}
-                  { ...props }
+                  {...props}
                 />
               );
             } else {
@@ -173,9 +143,7 @@ const ChildItem = ({ menuItem, level, ...props }) => {
             }
           })}
         </ul>
-      ) : (
-        null
-      )}
+      ) : null}
     </li>
   );
 };
@@ -183,20 +151,18 @@ const ChildItem = ({ menuItem, level, ...props }) => {
 /**
  * The placeholder skeleton that shows before query loads.
  */
-const Skeleton = ({ classNames, ignoreClasses, error }) => {
+const Skeleton = ({ error }) => {
   return Array.from(new Array(error?.message ? 1 : 5)).map(() => (
-    <li
-      key={Math.random()}
-      className={!ignoreClasses && classNames?.li?.length > 0 ? `menu-item level-1 ${classNames.li[0]}` : 'menu-item level-1'}
-    >
-      <span className={!ignoreClasses && classNames?.a?.length > 0 ? classNames.a[0] : ''} >
-        {error?.message
-        ? error.message
-        : <span className="h1 w3 ml2 loading-block dib" />
-        }
-      </span>
+    <li key={Math.random()}>
+      <a href="/">
+        {error?.message ? (
+          error.message
+        ) : (
+          <span className="h1 w3 ml2 loading-block dib" />
+        )}
+      </a>
     </li>
   ));
-}
+};
 
 export default Menu;
