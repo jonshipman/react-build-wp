6f37ca97decfcb0383cc5d632c2a33ec0afb546b
Jon Shipman <jon@jonshipman.com>
2020-07-10 11:47:44 -0500

    Cleanup apollo cache component

---
+++ react/src/components/elements/Cleanup.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useApolloClient } from "@apollo/client";
+
+import Config from "../../config";
+
+export const useCleanup = (redirect = "/login", types = []) => {
+  const history = useHistory();
+  const client = useApolloClient();
+
+  Config.removeAuthToken();
+
+  // Clear the sensitize caches
+  Object.keys(client.cache.data.data).forEach((key) =>
+    types.forEach(
+      (type) => 0 === key.indexOf(type) && client.cache.data.delete(key)
+    )
+  );
+
+  useEffect(() => {
+    history.push(redirect);
+  }, [redirect, history]);
+};
+
+export default ({ redirect, types }) => {
+  useCleanup(redirect, types);
+
+  return null;
+};
