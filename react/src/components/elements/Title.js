912fa2e5db06ef98e82f3d55a32706c9bb85afa4
Jon Shipman <jon@jonshipman.com>
2020-07-17 13:30:34 -0500

    refactor

--- react/src/components/elements/Title.js
+++ react/src/components/elements/Title.js
@@ -1,27 +1,20 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, createElement } from "react";
 
 import PageWidth from "./PageWidth";
 
-const classes = ["title ma0 lh-solid pv4", "f4 fw4 db"];
-
 export default forwardRef(
-  ({ notHeading, className, children, ...props }, ref) => (
-    <div className={`bg-near-white ${className || ""}`} {...props}>
-      <PageWidth>
-        {notHeading ? (
-          <div className={classes[0]}>
-            <span className={classes[1]} ref={ref}>
+  ({ notHeading, className = "", children, ...props }, ref) => {
+    const Wrap = createElement(notHeading ? "div" : "h1", {});
+    return (
+      <div className={`bg-near-white ${className}`} {...props}>
+        <PageWidth>
+          <Wrap.type className="title ma0 lh-solid pv4">
+            <span className="f4 fw4 db" ref={ref}>
               {children}
             </span>
-          </div>
-        ) : (
-          <h1 className={classes[0]}>
-            <span className={classes[1]} ref={ref}>
-              {children}
-            </span>
-          </h1>
-        )}
-      </PageWidth>
-    </div>
-  )
+          </Wrap.type>
+        </PageWidth>
+      </div>
+    );
+  }
 );
