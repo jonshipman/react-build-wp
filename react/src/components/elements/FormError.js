1b56c880b6fcc09649d5b79162115b5a8683625e
Jon Shipman <jon@jonshipman.com>
2020-07-28 13:17:32 -0600

    update

--- react/src/components/elements/FormError.js
+++ react/src/components/elements/FormError.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-const formError = (props) => {
-  const { children } = props;
-  return <div className="error-message red fw7 f7">{children}</div>;
-};
-
-export default formError;
+export default ({ children }) => (
+  <div className="error-message red fw7 f7">{children}</div>
+);
