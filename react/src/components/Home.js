f6d01af8a51f6c61eabd7896076b7d466331f060
Jon Shipman <jon@jonshipman.com>
2020-07-31 07:22:46 -0600

    removed post folder (most sites need custom)

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -3,16 +3,12 @@ import { Helmet } from "react-helmet";
 import LazyLoad from "react-lazy-load";
 import { gql, useQuery } from "@apollo/client";
 
-import { BlocksTwo, BlocksTwoFull } from "./elements/Blocks";
+import { BlocksTwo } from "./elements/Blocks";
 import Button from "./elements/Button";
-import CyclingCards from "./posts/CyclingCards";
 import Hero from "./elements/Hero";
 import Image from "./elements/Image";
-import LargeRow from "./posts/LargeRow";
 import LeadForm from "./elements/LeadForm";
 import PostContent from "./elements/PostContent";
-import SingleCard from "./posts/SingleCard";
-import TallCards from "./posts/TallCards";
 
 const HOME_QUERY = gql`
   query HomeQuery {
@@ -66,27 +62,6 @@ const OnQueryFinished = ({ seo, content, error }) => (
         }
       />
     </LazyLoad>
-
-    <LazyLoad>
-      <BlocksTwoFull
-        className="mv4"
-        left={
-          <SingleCard
-            heading="Coverage That Meets Your Needs"
-            subheading="We Provide The Best Service Hands Down"
-          />
-        }
-        right={<CyclingCards heading="Our Company Advantage" />}
-      />
-    </LazyLoad>
-
-    <LazyLoad>
-      <TallCards />
-    </LazyLoad>
-
-    <div className="mv4">
-      <LargeRow className="vh-50-l w-100 mw9 center" />
-    </div>
 
     <div className="bg-silver pv5">
       <LeadForm className="mw6 bg-white pa4 center" />
