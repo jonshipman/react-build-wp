0a23de6ffefea8d05ca9b45c44377701df463313
Jon Shipman <jon@jonshipman.com>
2020-08-18 12:56:51 -0500

    added a simple lazy loader

---
+++ react/src/components/elements/LazyLoad.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import scrollHandler from "../../handlers/scroll";
+
+export default function LazyLoad({ children }) {
+  const container = useRef();
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    function scrollDetection({ height }) {
+      if (container.current) {
+        const bounds = container.current.getBoundingClientRect();
+
+        if (bounds.top - height < 0) {
+          setVisible(true);
+        }
+      }
+    }
+
+    scrollHandler.add(scrollDetection);
+    return () => {
+      scrollHandler.remove(scrollDetection);
+    };
+  }, [container]);
+
+  if (!visible) {
+    return <div ref={container} />;
+  }
+
+  return <div ref={container}>{children}</div>;
+}
