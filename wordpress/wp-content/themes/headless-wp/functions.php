3a79ebc2e1a6482b0b2b22b02c09e7eb578602dd
Jon Shipman <jon@jonshipman.com>
2020-09-02 14:05:59 -0500

    remove acf class commands (legacy Postlight)

--- wordpress/wp-content/themes/headless-wp/functions.php
+++ wordpress/wp-content/themes/headless-wp/functions.php
@@ -13,9 +13,6 @@
 // Frontend origin.
 require_once 'inc/frontend-origin.php';
 
-// ACF commands.
-require_once 'inc/class-acf-commands.php';
-
 // Logging functions.
 require_once 'inc/log.php';
 
