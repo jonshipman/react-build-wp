60f568e27a5896a1d558ba156df5531efb783b4b
Jon Shipman <jon@jonshipman.com>
2020-09-04 06:46:20 -0500

    fixed contact

--- react/src/components/Contact.js
+++ react/src/components/Contact.js
@@ -1,37 +1,36 @@
 import React from "react";
 import { LeadForm } from "react-boilerplate-leadform";
 import {
-  NodeProvider,
   SingleRender,
   SingleTitle,
   ErrorRouting,
   useSingle,
-  Populate,
 } from "react-boilerplate-nodes";
 
 import { ButtonClasses } from "./elements/Button";
 
-const Contact = (props) => {
-  Populate(props);
+const Contact = () => {
   const { node, loading, error } = useSingle();
 
   return (
-    <NodeProvider value={props}>
+    <>
+      <SingleTitle {...node} />
       {loading || error || !node.id ? (
         <ErrorRouting loading={loading} error={error} />
       ) : (
-        <React.Fragment>
-          <SingleTitle {...node} />
+        <>
           <SingleRender node={node} />
           <div className="pv5">
             <LeadForm
               className="mw6 center"
-              classes={{ button: ButtonClasses.buttonType1 }}
+              classes={{
+                button: ButtonClasses.buttonType1,
+              }}
             />
           </div>
-        </React.Fragment>
+        </>
       )}
-    </NodeProvider>
+    </>
   );
 };
 
