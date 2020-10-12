c4873cc93d8a6b63938539329a32071cc2e9a8c7
Jon Shipman <jon@jonshipman.com>
2020-10-12 09:29:16 -0500

    🚦💾🚗🐹🍾🌟

--- react/src/config.js
+++ react/src/config.js
@@ -5,4 +5,4 @@ export const FRONTEND_URL = "http://localhost:3000";
 // Change to true to use the redirection query in ssr
 export const useRedirectionSsr = false;
 // GraphQL endpoint
-export const gqlUrl = BACKEND_URL + "/graphql",
+export const gqlUrl = BACKEND_URL + "/graphql";
