2a90577b5747f2dbddb0a6df607d2ad738eeb518
Jon Shipman <jon@jonshipman.com>
2020-10-20 13:16:44 -0500

    moar phpcs

--- wordpress/wp-content/themes/headless-wp/functions.php
+++ wordpress/wp-content/themes/headless-wp/functions.php
@@ -2,60 +2,69 @@
 /**
  * Theme for the Headless WordPress Starter Kit.
  *
- * Read more about this project at:
- * https://github.com/jonshipman/headless-wp-starter
- *
- * Originally forked from the fine folks from https://github.com/postlight/headless-wp-starter
+ * Local configuration hooks.
  *
  * @package  Headless_WP
  */
 
- // Filter to change origin.
- add_filter(
-	'frontend_origin',
-	function() {
-		return 'http://localhost:3000';
-	}
-);
-
-function headless_wp_settings_label() {
-	return __('Headless Settings', 'headless-wp' );
+/**
+ * Filter to change the origin.
+ *
+ * @return string
+ */
+function hewp_frontend_origin() {
+	return 'http://localhost:3000';
 }
 
-add_filter( 'wp_boilerplate_nodes_settings_page_label', 'headless_wp_settings_label' );
-add_filter( 'wp_boilerplate_nodes_settings_menu_label', 'headless_wp_settings_label' );
+add_filter( 'frontend_origin', 'hewp_frontend_origin' );
 
-// Filter to change local settings.
-add_filter(
-	'wp_boilerplate_nodes_settings',
-	function( $settings ) {
-		$theme_settings = array(
-			'company_info' => array(
-				'label'  => __( 'Company Info', 'headless-wp' ),
-				'fields' => array(
-					'phone_number'  => array(
-						'label' => __( 'Phone Number', 'headless-wp' ),
-						'args'  => array(
-							'type'              => 'string',
-							'sanitize_callback' => 'sanitize_text_field',
-							'show_in_graphql'   => true,
-						),
+/**
+ * Changes the settings label in WP Admin.
+ *
+ * @return string
+ */
+function hewp_settings_label() {
+	return __( 'Headless WordPress', 'headless-wp' );
+}
+
+add_filter( 'wp_boilerplate_nodes_settings_page_label', 'hewp_settings_label' );
+add_filter( 'wp_boilerplate_nodes_settings_menu_label', 'hewp_settings_label' );
+
+/**
+ * Adds theme specific settings to WP Admin.
+ *
+ * @param array $settings Settings to filter.
+ * @return array
+ */
+function hewp_settings_array( $settings ) {
+	$theme_settings = array(
+		'company_info' => array(
+			'label'  => __( 'Company Info', 'headless-wp' ),
+			'fields' => array(
+				'phone_number'  => array(
+					'label' => __( 'Phone Number', 'headless-wp' ),
+					'args'  => array(
+						'type'              => 'string',
+						'sanitize_callback' => 'sanitize_text_field',
+						'show_in_graphql'   => true,
 					),
-					'contact_email' => array(
-						'label' => __( 'Contact Email', 'headless-wp' ),
-						'args'  => array(
-							'type'              => 'string',
-							'sanitize_callback' => 'sanitize_text_field',
-							'show_in_graphql'   => true,
-						),
+				),
+				'contact_email' => array(
+					'label' => __( 'Contact Email', 'headless-wp' ),
+					'args'  => array(
+						'type'              => 'string',
+						'sanitize_callback' => 'sanitize_text_field',
+						'show_in_graphql'   => true,
 					),
 				),
 			),
-		);
+		),
+	);
 
-		return array_merge(
-			$settings,
-			$theme_settings
-		);
-	}
-);
+	return array_merge(
+		$settings,
+		$theme_settings
+	);
+}
+
+add_filter( 'wp_boilerplate_nodes_settings', 'hewp_settings_array' );
