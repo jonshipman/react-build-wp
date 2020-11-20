a65b558cac4c56db9f890c39190fe421c817dde4
Jon Shipman <jon@jonshipman.com>
2020-11-20 14:37:36 -0600

    Added preview parsing

--- react/src/App.js
+++ react/src/App.js
@@ -1,23 +1,28 @@
 // React
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import { WordPressRoutes } from "react-wp-gql";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { WordPressRoutes, Node } from "react-wp-gql";
 import { Header, Footer, Main } from "./layout";
 import { Home } from "./home";
 import "./app.scss";
 
-export const App = () => (
-  <div className="min-vh-100 flex items-stretch flex-column w-100 sans-serif near-black">
-    <Header />
-    <Main>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+export const App = () => {
+  const { search } = useLocation();
+  const previewId = new URLSearchParams(search).get("p");
 
-        <WordPressRoutes />
-      </Switch>
-    </Main>
-    <Footer />
-  </div>
-);
+  return (
+    <div className="min-vh-100 flex items-stretch flex-column w-100 sans-serif near-black">
+      <Header />
+      <Main>
+        <Switch>
+          <Route exact path="/">
+            {previewId ? <Node databaseId={previewId} /> : <Home />}
+          </Route>
+
+          <WordPressRoutes />
+        </Switch>
+      </Main>
+      <Footer />
+    </div>
+  );
+};
