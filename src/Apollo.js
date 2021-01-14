cc644edfc241d9cc748bc0e6568d3c4d105e9a8e
Jon Shipman <jon@jonshipman.com>
2021-01-14 16:01:42 -0600

    moved Apollo Setup from react-wp-gql to boilerplate

--- src/Apollo.js
+++ src/Apollo.js
@@ -1,5 +1,29 @@
-import { InMemoryCache } from "@apollo/client";
-export const cache = new InMemoryCache({
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider as Provider,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+  from,
+} from "@apollo/client";
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const cache = new InMemoryCache({
   typePolicies: {
     Post: {
       fields: {
@@ -7,10 +31,36 @@ export const cache = new InMemoryCache({
           read: function (date) {
             const d = new Date(date);
 
-            return `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}`;
+            return `${
+              monthNames[d.getMonth()]
+            } ${d.getDate()}, ${d.getFullYear()}`;
           },
         },
       },
     },
   },
 });
+
+const authAfterware = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    // If we get an error, log the error.
+    if (response?.errors?.length > 0) {
+      console.error(response.errors);
+    }
+
+    return response;
+  });
+});
+
+const link = new HttpLink({
+  uri: `${process.env.REACT_APP_DOMAIN}/graphql`,
+});
+
+export const ApolloProvider = ({ children }) => {
+  const client = new ApolloClient({
+    link: from([authAfterware, link]),
+    cache,
+  });
+
+  return <Provider {...{ client }}>{children}</Provider>;
+};
--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { NodeProvider } from "react-wp-gql";
 import { ScrollToTop } from "./components";
-import { cache } from "./Apollo";
+import { ApolloProvider } from "./Apollo";
 import * as fragments from "./gql/fragments";
 import reportWebVitals from "./reportWebVitals";
 
 const nodeProps = {
   siteName: "React Build",
-  gqlUrl: `${process.env.REACT_APP_DOMAIN}/graphql`,
-  cache,
   fragments,
 };
 
 ReactDOM.render(
   <BrowserRouter>
     <ScrollToTop />
-    <NodeProvider {...nodeProps}>
-      <App />
-    </NodeProvider>
+    <ApolloProvider>
+      <NodeProvider {...nodeProps}>
+        <App />
+      </NodeProvider>
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
